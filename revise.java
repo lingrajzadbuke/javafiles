b88c5b6df53572b1a549e14cb2e797992d62f2af
lingraj zadbuke <lingrajzadbuke@gmail.com>
2026-08-16 12:28:09 +0530

    revise

---
+++ revise.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class revise {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("please enter a number: ");
+        int x = sc.nextInt();
+
+        System.out.println("incrementing the number by 1: " + (x + 1));
+    }
+
+}
